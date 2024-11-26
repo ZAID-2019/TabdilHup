f06f1fc8778b9b93d2b4bbd7c9d5c9122ab82943
IZZO94 <abo.z1999@gmail.com>
2024-11-26 03:41:44 +0300

    Add Cloudinary integration for image uploads with Upload module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,11 @@ import { CitiesModule } from './cities/cities.module';
 import { ItemsModule } from './items/items.module';
 import { CategoriesModule } from './categories/categories.module';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { UploadModule } from './upload/upload.module';
 
 @Module({
-  imports: [UsersModule, PrismaModule, AuthModule, CountriesModule, CitiesModule, ItemsModule, CategoriesModule, SubscriptionsModule],
+  imports: [UsersModule, PrismaModule, AuthModule, CountriesModule, CitiesModule, ItemsModule, CategoriesModule, SubscriptionsModule, CloudinaryModule, UploadModule],
   controllers: [],
   providers: [],
 })
---
+++ src/cloudinary/cloudinary.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { UploadApiResponse, UploadApiErrorResponse } from 'cloudinary';
+
+@Injectable()
+export class CloudinaryService {
+
+
+    constructor() {
+        cloudinary.config({
+            cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+            api_key: process.env.CLOUDINARY_API_KEY,
+            api_secret: process.env.CLOUDINARY_API_SECRET,
+        });
+    }
+
+    async uploadImage(file: Express.Multer.File): Promise<UploadApiResponse | UploadApiErrorResponse> {
+        console.log("file 2", file);
+        
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload_stream(
+                { folder: 'uploads' },
+                (error, result) => {
+                    if (error) return reject(error);
+                    resolve(result);
+                },
+            ).end(file.buffer);
+        });
+    }
+}
